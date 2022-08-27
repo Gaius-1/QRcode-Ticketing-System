48771e96ed6440790aa7ae1b4a9c54d1dd4b5c18
Gaius-1 <agbofrederick230@gmail.com>
2022-08-27 20:49:12 +0000

    configure server

--- src/config.py
+++ src/config.py
@@ -3,8 +3,8 @@
 INPUT_FILE_PATH = '../assets/Participants.xlsx'
 OUTPUT_FILE_PATH = '../assets/Approved_Guests.xlsx'
 ADMIN_PASSWORD = '1234'
-# DOMAIN_NAME = 'http://127.0.0.1:8000'
-DOMAIN_NAME = 'https://8000-sadjadeb-qrcodeticket-g1rrlm82bsd.ws-eu62.gitpod.io/'
+DOMAIN_NAME = 'http://127.0.0.1:8000'
+# DOMAIN_NAME = 'https://8000-gaius1-qrcodeticketings-256zdob18db.ws-eu63.gitpod.io'
 
 PAGE_TITLE = 'QR Ticket'
 EVENT_NAME = 'Circum Mundum, Dinner & Awards Night'
